fa88c4bad067e84c2147be54e09faba366a0e065
MattiaIovine <76738748+MattiaIovine@users.noreply.github.com>
2020-12-30 17:33:31 +0100

    Update Persona.java

--- Programma/src/Persona.java
+++ Programma/src/Persona.java
@@ -1,4 +1,13 @@
-
 public class Persona {
+	
+	//ATTRIBUTI 
+	public String CF;
+	public String Nome;
+	public String Cognome;
+	public float  Salario;
+	public int    Valutazione;
+	public String Mail;
+	public int    Telefono;
+	
 	
 }
