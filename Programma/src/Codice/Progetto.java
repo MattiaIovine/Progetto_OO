65ebc2ccc218ad9446c2512f1249c2e8303604df
felice267 <76739319+felice267@users.noreply.github.com>
2021-01-14 17:42:19 +0100

    Update Progetto.java

--- Programma/src/Codice/Progetto.java
+++ Programma/src/Codice/Progetto.java
@@ -7,6 +7,27 @@ public class Progetto {
 	public String ID_Progetto;
 	public String Tipologia;
 	public String Ambito;
-
 	public ArrayList<Persona> Componenti;
+	//GETTER E SETTER
+	public String getID_Progetto() {
+		return ID_Progetto;
+	}
+	public void setID_Progetto(String iD_Progetto) {
+		ID_Progetto = iD_Progetto;
+	}
+	public String getTipologia() {
+		return Tipologia;
+	}
+	public void setTipologia(String tipologia) {
+		Tipologia = tipologia;
+	}
+	public String getAmbito() {
+		return Ambito;
+	}
+	public void setAmbito(String ambito) {
+		Ambito = ambito;
+	}
+
+
+
 }
