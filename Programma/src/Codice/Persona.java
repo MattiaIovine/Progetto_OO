be81212be61013b17c9c9f9cbcf15019822d6180
GaBelardo <76739279+GaBelardo@users.noreply.github.com>
2021-01-14 17:40:13 +0100

    Update Persona.java

--- Programma/src/Codice/Persona.java
+++ Programma/src/Codice/Persona.java
@@ -16,7 +16,58 @@ public class Persona {
 	public ArrayList<Progetto> Lavori;
 	public ArrayList<Meeting> Partecipazioni;
 	public ArrayList<Skill> Abilità;
-
+	
+	
+	
+	//METODI
+	public String getCF() {
+		return CF;
+	}
+	public void setCF(String cF) {
+		CF = cF;
+	}
+	
+	public String getNome() {
+		return Nome;
+	}
+	public void setNome(String nome) {
+		Nome = nome;
+	}
+	
+	public String getCognome() {
+		return Cognome;
+	}
+	public void setCognome(String cognome) {
+		Cognome = cognome;
+	}
+	
+	public String getMail() {
+		return Mail;
+	}
+	public void setMail(String mail) {
+		Mail = mail;
+	}
+	
+	public int getTelefono() {
+		return Telefono;
+	}
+	public void setTelefono(int telefono) {
+		Telefono = telefono;
+	}
+	
+	public float getSalario() {
+		return Salario;
+	}
+	public void setSalario(float salario) {
+		Salario = salario;
+	}
+	
+	public int getValutazione() {
+		return Valutazione;
+	}
+	public void setValutazione(int valutazione) {
+		Valutazione = valutazione;
+	}
 	
 	
 }
