ff2dbce5efca345a196a88aa5a043fa2a2aaa19e
MattiaIovine <76738748+MattiaIovine@users.noreply.github.com>
2021-01-14 17:39:25 +0100

    Update Meeting.java
    getter e setter

--- Programma/src/Codice/Meeting.java
+++ Programma/src/Codice/Meeting.java
@@ -12,4 +12,30 @@ public class Meeting {
 	public Progetto Argomento;
 	public ArrayList<Persona> Partecipanti;
 	
+	//GETTER e SETTER
+	public String getID_Meeting() {
+		return ID_Meeting;
+	}
+	public void setID_Meeting(String iD_Meeting) {
+		ID_Meeting = iD_Meeting;
+	}
+	public Date getData() {
+		return Data;
+	}
+	public void setData(Date data) {
+		Data = data;
+	}
+	public Time getOrario() {
+		return Orario;
+	}
+	public void setOrario(Time orario) {
+		Orario = orario;
+	}
+	public Progetto getArgomento() {
+		return Argomento;
+	}
+	public void setArgomento(Progetto argomento) {
+		Argomento = argomento;
+	}
+	
 }
